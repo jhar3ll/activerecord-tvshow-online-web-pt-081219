2c4895d93dfa1fe26d2161ca26444f9d5e0d8b20
Jharell Chambers <jharell.chambers@selu.edu>
2019-11-20 18:18:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,10 @@
 class Show < ActiveRecord::Base 
   
   def self.highest_rating 
-    self.
+    self.maximum(ratings)
+  end 
+  
+  def self.most_popular_show 
+    self.highest_rating
+  end 
 end 
